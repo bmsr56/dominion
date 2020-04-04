f50e9b88eaee590633b1ac63efcb0375fcd1b2b3
Ben Simpson <simpsy.b@gmail.com>
2020-04-04 15:34:32 -0500

    First draft of classes to define cards

---
+++ cards.py
@@ -0,0 +1,30 @@
+class CardTemplate:
+    """ Base class for a Dominion Card.
+    """
+    def __init__(self):
+        self.title =
+        self.types
+        self.parentSet
+        self.
+    
+    def actions():
+        pass
+
+    def draw():
+        pass
+    
+
+    # 
+    def print(self) -> None:
+        """ Print the card in a human-readable format
+        """
+        pass
+
+class Card(CardTemplate):
+    
+    def __init__(self):
+        super().__init__()
+    
+
+if __name__ == '__main__':
+    main()
